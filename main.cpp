cae0b0bce6b9a552a158a6aee913aca7d935614e
Adzkia Anfariza Rizqika <5024241003@student.its.ac.id>
2025-11-24 01:31:30 +0700

    added more balls

--- main.cpp
+++ main.cpp
@@ -1,41 +1,67 @@
 #include "raylib.h"
 #include <vector>
-#include <string.h>
 #include <iostream>
 
 using namespace std;
+
 int panjang = 800;
 int lebar = 600;
-int centerX = 400;
-int centerY = 300;
-int speedX = 4;
-int speedY = 5;
-void circlemovement(int &speedx, int &speedy, int radius, int &centerX, int &centerY){
-        centerX += speedx;
-        centerY += speedy;
-    DrawCircle (centerX, centerY, radius, RED);
+const int radius = 50;
+int ballCount = 10;
+
+void circlemovement(int &speedx, int &speedy, int radius, Vector2 &loc){
+    loc.x += speedx;
+    loc.y += speedy;
+    DrawCircle(loc.x, loc.y, radius, RED);
 }
 
-void BoundaryCircle(int &centerX, int &centerY, int &speedx, int &speedy, int radius){
-    if (centerX + radius >= panjang || centerX - radius <= 0){
+void BoundaryCircle(Vector2 &loc, int &speedx, int &speedy, int radius){
+    if (loc.x + radius >= panjang || loc.x - radius <= 0){
         speedx = -speedx;
     }
-    if (centerY + radius >= lebar || centerY - radius <= 0){
+    if (loc.y + radius >= lebar || loc.y - radius <= 0){
         speedy = -speedy;
     }
 }
 
-
-int main (){
+int main(){
     InitWindow(panjang, lebar, "Collision Detection");
     SetTargetFPS(60);
-    while (!WindowShouldClose()){
+
+
+    vector<Vector2> loc;
+    vector<int> speedX;
+    vector<int> speedY;
+
+    for (int i = 0; i < ballCount; i++){
+        // randomize posisi
+        Vector2 tempPos = {
+            (float)GetRandomValue(radius, panjang - radius),
+            (float)GetRandomValue(radius, lebar - radius)
+        };
+        loc.push_back(tempPos);
+
+        // randomize speed
+        speedX.push_back(GetRandomValue(-5, 5));
+        speedY.push_back(GetRandomValue(-5, 5));
+
+        if (speedX[i] == 0) speedX[i] = 2;
+        if (speedY[i] == 0) speedY[i] = 2;
+    }
+
+
+    while (!WindowShouldClose()) {
         BeginDrawing();
         ClearBackground(RAYWHITE);
-        circlemovement(speedX, speedY, 50, centerX, centerY);
-        BoundaryCircle(centerX, centerY, speedX, speedY, 50);
+
+        for (int i = 0; i < ballCount; i++){
+            circlemovement(speedX[i], speedY[i], radius, loc[i]);
+            BoundaryCircle(loc[i], speedX[i], speedY[i], radius);
+        }
+
         EndDrawing();
     }
+
     CloseWindow();
     return 0;
 }
